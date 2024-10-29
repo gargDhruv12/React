9ec7044f5ab40b6df9b5f8f212a6a2fc4e0969d7
gargDhruv12 <dhruvgarg729729@gmail.com>
2024-10-29 23:17:47 +0530

    Navbar and Textarea learnt

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,17 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+// import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React, { useState } from 'react'
+import Alert from './Alert';
 
 function App() {
+  const [mode,setMode] = useState("light")
+    const toggleMode = ()=>{
+      if(mode === 'dark'){
+        setMode('light');
+        document.body.style.backgroundColor = "white";
+      }
+      else{
+        setMode('dark');
+        document.body.style.backgroundColor = "#0c1f31";
+      }
+    }
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
+    // <div classNameName="App">
+    //   <header classNameName="App-header">
+    //     <img src={logo} classNameName="App-logo" alt="logo" />
     //     <p>
     //       Edit <code>src/App.js</code> and save to reload.
     //     </p>
     //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
+    //       classNameName="App-link"
+    //      href="https://reactjs.org"
     //       target="_blank"
     //       rel="noopener noreferrer"
     //     >
@@ -19,7 +35,19 @@ function App() {
     //     </a>
     //   </header>
     // </div>
-    <div> Hello, Eager to learn react quickly</div>
+
+    // <div> Hello, Eager to learn react quickly</div>
+    
+    <>
+      
+      <Navbar title = "TextUtils" AboutText = "About us" mode= {mode} toggleMode={toggleMode}/>
+      <Alert alert="I am alert"/>
+      <div className='container my-3'>
+        <TextForm heading="Enter text here to analyze" mode= {mode}></TextForm >
+        {/* <About/> */}
+      </div>
+      
+    </>
   );
 }
 
---
+++ basics/src/components/TextForm.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react'
+
+export default function TextForm(props) {
+  const [text, setText] = useState("Enter text here");
+   const handleUpClick = ()=>{
+    // console.log("Uppercase was clicked " + text);
+    let newtext = text.toUpperCase();
+    setText(newtext);
+   }
+   const handleLoClick = ()=>{
+    // console.log("lowercase was clicked " + text);
+    let newtext = text.toLowerCase();
+    setText(newtext);
+   }
+   const handleOnchange= (event)=>{
+    // console.log("On change");
+    setText(event.target.value);
+   }
+  return (
+    <>
+    <div className='container' style={{color : props.mode==="dark" ? "white" : "#0c1f31" }}>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+            <textarea className="form-control" value={text} onChange={handleOnchange} id="mybox" rows="5" style={{
+              backgroundColor : props.mode==="dark" ? "grey" : "white",
+              color : props.mode==="dark" ? "white" : "#0c1f31"
+            }}></textarea>
+        </div>
+        <button className='btn btn-primary mx-1' onClick={handleUpClick}>Convert to uppercase</button>
+        <button className='btn btn-primary mx-1' onClick={handleLoClick}>Convert to lowercase</button>
+        
+    </div>
+    <div className='container my-3' style={{color : props.mode==="dark" ? "white" : "#0c1f31" }}>
+        <h2>Text Summary</h2>
+        <p>total {text.split(" ").length} words , {text.length} characters</p>
+        <p>Readable in {0.008 * text.split(" ").length} minutes </p>
+    </div>
+    </>
+  )
+}
